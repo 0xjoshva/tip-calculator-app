3bfd528f3aa81b691edd4a20f48038d045ffe83d
0xjoshva <joshuasteed123@gmail.com>
2022-10-31 16:15:18 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,47 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import url('https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+/* font-family: 'Space Mono', monospace; */
+
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+* {
+  margin: 0;
+}
+html, body {
+  height: 100%;
+}
+body {
+  line-height: 1.5;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
+img, picture, video, canvas, svg {
+  display: block;
+  max-width: 100%;
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+input, button, textarea, select {
+  font: inherit;
 }
+p, h1, h2, h3, h4, h5, h6 {
+  overflow-wrap: break-word;
+}
+#root, #__next {
+  isolation: isolate;
+}
+:root{
+  /* primary */
+  --scyan: hsl(172, 67%, 45%);
 
-nav a.router-link-exact-active {
-  color: #42b983;
+  /* neutral */
+  --vdcyan: hsl(183, 100%, 15%);
+  --dgcyan: hsl(186, 14%, 43%);
+  --gcyan: hsl(184, 14%, 56%);
+  --lgcyan: hsl(185, 41%, 84%);
+  --vlgcyan: hsl(189, 41%, 97%);
+  --white: hsl(0, 0%, 100%);
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,200 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section>
+    <h1>
+      SPLI <br />
+      TTER
+    </h1>
+    <div class="container">
+      <div class="left">
+        <label for="">
+          <p>Bill</p>
+          <input type="text" placeholder="0" />
+        </label>
+        <label for="">
+          <p>Select Tip%</p>
+          <div class="radio-buttons">
+            <div class="radiodiv">
+              <button
+                class="radio-btn"
+                value="5"
+                @click="(isChecked = true), (tip = 5)"
+                :class="{ checked: tip === 5 }"
+              >
+                5%
+              </button>
+              <button
+                class="radio-btn"
+                value="10"
+                @click="(isChecked = true), (tip = 10)"
+                :class="{ checked: tip === 10 }"
+              >
+                10%
+              </button>
+              <button
+                class="radio-btn"
+                value="15"
+                @click="(isChecked = true), (tip = 15)"
+                :class="{ checked: tip === 15 }"
+              >
+                15%
+              </button>
+            </div>
+
+            <div class="radiodiv">
+              <button
+                class="radio-btn"
+                value="25"
+                @click="(isChecked = true), (tip = 25)"
+                :class="{ checked: tip === 25 }"
+              >
+                25%
+              </button>
+              <button
+                class="radio-btn"
+                value="50"
+                @click="(isChecked = true), (tip = 50)"
+                :class="{ checked: tip === 50 }"
+              >
+                50%
+              </button>
+              <button class="radio-btn custom">Custom</button>
+            </div>
+          </div>
+        </label>
+        <label for="">
+          <p>Number of People</p>
+          <input type="text" placeholder="0" />
+        </label>
+      </div>
+      <div class="right">
+        <div class="tipamount">
+          <input type="text" placeholder="0.00"/>
+        </div>
+        <div></div>
+        <button class="reset-btn" :disabled="isDisabled">RESET</button>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      tip: 0,
+      isChecked: false,
+      isDisabled: true,
+    };
+  },
+};
 </script>
+
+<style>
+section {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  background: var(--lgcyan);
+  flex-direction: column;
+  gap: 4rem;
+  font-family: "Space Mono", monospace;
+}
+h1 {
+  letter-spacing: 6px;
+  color: var(--dgcyan);
+  font-size: 28px;
+}
+.container {
+  background: var(--white);
+  border-radius: 26px;
+  width: 60rem;
+  height: 30rem;
+  box-shadow: 0px 0px 30px 0px rgba(0, 0, 0, 0.13);
+  display: flex;
+  flex-direction: row;
+  padding: 2rem;
+  justify-content: space-between;
+  padding-left: 3rem;
+  column-gap: 2rem;
+}
+.left {
+  width: 50%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  color: var(--dgcyan);
+  font-weight: 700;
+  justify-content: space-between;
+  padding-bottom: 1rem;
+  padding-top: 1rem;
+}
+
+.radio-buttons {
+  display: flex;
+  flex-direction: column;
+  row-gap: 1rem;
+}
+.radio-btn {
+  font-size: 24px;
+  font-weight: 700;
+  color: var(--vlgcyan);
+  background: var(--vdcyan);
+  border: none;
+  outline: none;
+  border-radius: 5px;
+  width: 30%;
+  padding-block: 0.4rem;
+}
+.radiodiv {
+  display: flex;
+  column-gap: 1rem;
+}
+.left label {
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.5rem;
+}
+.left input {
+  font-size: 24px;
+  border: none;
+  outline: none;
+  width: 100%;
+  background: var(--vlgcyan);
+  border-radius: 5px;
+  padding: 0.2rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+}
+.checked {
+  color: var(--vdcyan);
+  background: var(--scyan);
+}
+.right {
+  width: 50%;
+  height: 100%;
+  background: var(--vdcyan);
+  border-radius: 20px;
+  padding: 2rem;
+}
+.reset-btn {
+  width: 100%;
+  border-radius: 6px;
+  font-size: 24px;
+  font-weight: 700;
+  border: none;
+  outline: none;
+  padding-block: .3rem;
+}
+.reset-btn:enabled {
+  color: var(--vdcyan);
+  background: var(--scyan);
+}
+.reset-btn:disabled {
+  filter: opacity(0.4);
+}
+.reset-btn:enabled:hover {
+  background: var(--lgcyan);
+}
+</style>
